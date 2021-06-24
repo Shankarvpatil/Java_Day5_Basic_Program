3c558ee89b38457886d6d0b51ff8255f812cccb7
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:12:08 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -2,20 +2,13 @@ package com.Bridglab.programs;
 
 import com.bridgebaz.utility.Utility;
 
-public class LargestAmongThree {
-
-	public static void main(String[] args) {
-		Utility utility = new Utility();
-		System.out.print("Enter the number num1 : ");
-		int num1 = utility.getIntValue();
-		System.out.print("Enter the number num2 : ");
-		int num2 = utility.getIntValue();
-		System.out.print("Enter the number num3 : ");
-		int num3 = utility.getIntValue();
-		utility.getLargestNumber(num1, num2, num3);
+public class LeapYear {
 	
-		
-
+	public static void main(String args[]) {
+		Utility utility = new Utility();
+		System.out.print("Enter the year to check is it is leap or not : ");
+		int ValueOfYear = utility.getIntValue();
+		utility.checkLeapYear(ValueOfYear);
 	}
 
 }
