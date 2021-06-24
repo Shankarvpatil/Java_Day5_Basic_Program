fa103416300661ac69dada9e50d6e567a715c536
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:14:45 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -2,13 +2,13 @@ package com.Bridglab.programs;
 
 import com.bridgebaz.utility.Utility;
 
-public class PowerOfTwo {
+public class PrimeFactors {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		Utility utility = new Utility();
-		System.out.print("Enter the Number to check power of two : ");
+		System.out.print("Enter the number to check primefactors : ");
 		int Number = utility.getIntValue();
-		utility.getPower(Number);
+		utility.getPrimeFactor(Number);
 	}
 
 }
