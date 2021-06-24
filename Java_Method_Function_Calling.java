01c2d82f1d1ae7a9b54ae4b3bb3aa5014619bc8f
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:10:40 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -2,13 +2,20 @@ package com.Bridglab.programs;
 
 import com.bridgebaz.utility.Utility;
 
-public class HarmonicNumber {
+public class LargestAmongThree {
 
 	public static void main(String[] args) {
 		Utility utility = new Utility();
-		System.out.print("Enter the number to check Harmonic number : ");
-		int Number = utility.getIntValue();
-		utility.getHarmonicNumber(Number);;
+		System.out.print("Enter the number num1 : ");
+		int num1 = utility.getIntValue();
+		System.out.print("Enter the number num2 : ");
+		int num2 = utility.getIntValue();
+		System.out.print("Enter the number num3 : ");
+		int num3 = utility.getIntValue();
+		utility.getLargestNumber(num1, num2, num3);
+	
+		
+
 	}
 
 }
