30edcb9d396c4590117df959e50c68525c9ad7ae
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:18:03 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -2,16 +2,16 @@ package com.Bridglab.programs;
 
 import com.bridgebaz.utility.Utility;
 
-public class QuotientAndRemainder {
-
+public class SwapTheNumber {
+	
 	public static void main(String[] args) {
-		Utility utility = new Utility();
-		System.out.print("Enter the Devident: ");
-		int Devident = utility.getIntValue();
-		System.out.print("Enter the Devisor : ");
-		int Devisor = utility.getIntValue();
-		utility.getQuotientRemainder(Devisor, Devident);
 		
-	}
+		Utility utility = new Utility();
+		System.out.print("Enter first number num1 : ");
+		int num1 = utility.getIntValue();
+		System.out.print("Enter first number num2 : ");
+		int num2 = utility.getIntValue();
+		utility.SwapNumber(num1,num2);
+		}
 
 }
