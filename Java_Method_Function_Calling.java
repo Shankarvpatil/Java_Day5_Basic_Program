be995103d01b1f3b34846dee76f94522bae9de5f
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 22:16:56 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -2,13 +2,16 @@ package com.Bridglab.programs;
 
 import com.bridgebaz.utility.Utility;
 
-public class PrimeFactors {
+public class QuotientAndRemainder {
 
 	public static void main(String[] args) {
 		Utility utility = new Utility();
-		System.out.print("Enter the number to check primefactors : ");
-		int Number = utility.getIntValue();
-		utility.getPrimeFactor(Number);
+		System.out.print("Enter the Devident: ");
+		int Devident = utility.getIntValue();
+		System.out.print("Enter the Devisor : ");
+		int Devisor = utility.getIntValue();
+		utility.getQuotientRemainder(Devisor, Devident);
+		
 	}
 
 }
