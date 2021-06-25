b3a88cece55d99a81895fc7e83555ee78e2e44c9
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 11:21:37 +0530

    Update Java_Method_Function_Calling.java

--- Java_Method_Function_Calling.java
+++ Java_Method_Function_Calling.java
@@ -7,8 +7,8 @@ public class PowerOfTwo {
 	public static void main(String args[]) {
 		Utility utility = new Utility();
 		System.out.print("Enter the Number to check power of two : ");
-		int Number = utility.getIntValue();
-		utility.getPower(Number);
+		int N = utility.getIntValue();
+		utility.getPower(N);
 	}
 
 }
